6ef565b84abe81f45d987af9255d8b2a52a79006
Evlogi Aleksandrov <evlogi.aleksandrov@gmail.com>
2018-10-22 21:11:04 +0300

    initial commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,58 @@
+// const MongoClient = require('mongodb').MongoClient;
+const { MongoClient, ObjectId } = require('mongodb');
+
+MongoClient.connect(
+	'mongodb://localhost:27017/TodoApp',
+	{ useNewUrlParser: true },
+	(err, client) => {
+		if (err) {
+			return console.log('Unable to connect to MongoDB server');
+		}
+		console.log('Connected to MongoDB server.');
+		const db = client.db('TodoApp');
+
+		// db.collection('Todos')
+		// 	.find({
+		// 		_id: new ObjectId('5bcdeb61e90cb7250a827c22'),
+		// 	})
+		// 	.toArray()
+		// 	.then(
+		// 		docs => {
+		// 			console.log('Todos:');
+		// 			console.log(JSON.stringify(docs, undefined, 2));
+		// 		},
+		// 		err => {
+		// 			console.log('Unable to fetch todos.', err);
+		// 		}
+		// 	);
+
+		// db.collection('Todos')
+		// 	.find()
+		// 	.count()
+		// 	.then(
+		// 		count => {
+		// 			console.log(`Todos count: ${count}`);
+		// 		},
+		// 		err => {
+		// 			console.log('Unable to fetch todos.', err);
+		// 		}
+		// 	);
+
+		db.collection('Users')
+			.find({
+				name: 'Evlogi',
+			})
+			.toArray()
+			.then(
+				docs => {
+					console.log('Todos:');
+					console.log(JSON.stringify(docs, undefined, 2));
+				},
+				err => {
+					console.log('Unable to fetch todos.', err);
+				}
+			);
+
+		//client.close();
+	}
+);
