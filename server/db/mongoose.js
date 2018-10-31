05594285f2ee1434752a5ad2073b5ead0f992253
Evlogi Aleksandrov <evlogi.aleksandrov@gmail.com>
2018-10-31 20:59:29 +0200

    fixed mongoURI

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 mongoose.connect(
-	'mongodb://<featr>:<jenifer727>@ds227469.mlab.com:27469/todo-api' ||
+	'mongodb://featr:jenifer727@ds227469.mlab.com:27469/todo-api' ||
 		'mongodb://localhost:27017/TodoApp',
 	{ useNewUrlParser: true }
 );
