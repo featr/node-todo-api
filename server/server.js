2f36c672a5c547d6db0048f4419e24a91c42e6f8
Evlogi Aleksandrov <evlogi.aleksandrov@gmail.com>
2018-11-06 22:20:00 +0200

    Add POST /users/login route

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { ObjectID } = require('mongodb');
 const _ = require('lodash');
+const bcrypt = require('bcryptjs');
 
 const { mongoose } = require('./db/mongoose');
 const { Todo } = require('./models/todo');
@@ -120,6 +121,19 @@ app.post('/users', (req, res) => {
 
 app.get('/users/me', authenticate, (req, res) => {
 	res.send(req.user);
+});
+
+app.post('/users/login', (req, res) => {
+	const body = _.pick(req.body, ['email', 'password']);
+	User.findByCredentials(body.email, body.password)
+		.then(user => {
+			user.generateAuthToken().then(token => {
+				res.header('x-auth', token).send(user);
+			});
+		})
+		.catch(e => {
+			res.status(400).send();
+		});
 });
 
 app.listen(port, () => {
